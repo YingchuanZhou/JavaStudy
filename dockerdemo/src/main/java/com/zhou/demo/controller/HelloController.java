448b9be2e5b6d84920141743dd4c47436c4c022d
YingchuanZhou <766623393@qq.com>
2021-10-12 22:21:49 +0800

    2021/10/12

---
+++ dockerdemo/src/main/java/com/zhou/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.zhou.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/hello")
+    public String hello(){
+        Long views = redisTemplate.opsForValue().increment("views");
+        return "Hello, thank you！ "+views;
+    }
+}
